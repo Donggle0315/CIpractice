8215922a9352f83b22a6b909cfe42947f6892932
DongHyun <kimdh0315@naver.com>
2024-11-30 20:04:21 +0900

    sixth commit

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from './config/typeOrmConfig';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import redisConfig from './config/redisConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    RedisModule.forRoot(redisConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/config/redisConfig.ts
@@ -0,0 +1,14 @@
+import { RedisModuleOptions } from '@liaoliaots/nestjs-redis';
+// 지워야됨
+// import './setEnviorment';
+
+const redisConfig: RedisModuleOptions = {
+  readyLog: true,
+  config: {
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT, 10),
+    password: process.env.REDIS_PASSWORD,
+  },
+};
+
+export default redisConfig;
